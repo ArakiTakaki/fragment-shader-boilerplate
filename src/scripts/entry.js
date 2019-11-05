e87321b1228453f6ed59079c73f7faf20bf0ccd6
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-11-05 15:31:27 +0900

    ちょっと調整

--- src/scripts/entry.js
+++ src/scripts/entry.js
@@ -6,9 +6,8 @@ import BaseObject from './objects/base';
 
 const elCanvas = document.createElement('canvas');
 document.getElementById(DOM.MAIN_CONTAINER).appendChild(elCanvas);
+
 const renderer = new THREE.WebGLRenderer({ canvas: elCanvas });
-
-
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 3;
 
@@ -17,8 +16,8 @@ resize({
   camera: camera,
 });
 const scene = new THREE.Scene();
-
 scene.add(BaseObject.mesh);
+
 const render = () => {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
